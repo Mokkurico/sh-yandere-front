4f600086731fd70af73485cbf49443e17d121eb1
i15317 <30828280+notchman8600@users.noreply.github.com>
2021-08-22 10:21:02 +0900

    モーダルのコンポーネントを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,31 @@ import Head from 'next/head';
 import React, { useState } from 'react';
 import TaskList from '../components/TaskList';
 import axios from 'axios';
-import { TaskItemStruct, TaskFilterStruct, PostTaskReadStruct } from '../_types';
+import { PostTaskReadStruct, TaskFilterStruct, TaskItemStruct } from '../_types';
 import { styleSceneTodoApp } from '../_styles';
 import { tmpUserId } from '../_constParams';
+import Modal from 'react-modal';
 
+// スタイリング
+const customStyles = {
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,0.3)',
+  },
+
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    width: '500px',
+    height: '300px',
+    transform: 'translate(-50%, -50%)',
+  },
+};
 const App: NextPage = () => {
   const styles = styleSceneTodoApp();
 
@@ -53,6 +74,25 @@ const App: NextPage = () => {
     };
   };
 
+  // アプリのルートを識別するクエリセレクタを指定する。
+  Modal.setAppElement('#__next');
+
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  // モーダルを開く処理
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const afterOpenModal = () => {
+    // モーダルが開いた後の処理
+  };
+
+  // モーダルを閉じる処理
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <Head>
@@ -80,6 +120,20 @@ const App: NextPage = () => {
         />
         {/*</Grid>*/}
       </div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        // isOpenがtrueならモダールが起動する
+        isOpen={modalIsOpen}
+        // モーダルが開いた後の処理を定義
+        onAfterOpen={afterOpenModal}
+        // モーダルを閉じる処理を定義
+        onRequestClose={closeModal}
+        // スタイリングを定義
+        style={customStyles}
+      >
+        <h2>Hello</h2>
+        <button onClick={closeModal}>close</button>
+      </Modal>
     </>
   );
 };
